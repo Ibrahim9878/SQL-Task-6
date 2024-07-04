78e1dea100eb56330b347bb7e0c81edbba52b04a
Ibrahim9878 <talibliibrahim28@gmail.com>
2024-07-04 15:45:25 +0400

    sql task 6

---
+++ TASK 6 SQL.sql
@@ -0,0 +1,13 @@
+--Teacher Post cedvelleri yaratdiqimiz tapshiriq ucucn ashaqida gosterilen view-lari yaradin:
+--16.1. All job titles.
+USE TEACHERS
+CREATE VIEW JobTitles
+AS
+SELECT PName
+FROM Posts 
+--16.2. All the names of teachers.
+CREATE VIEW NameOfTeachers
+AS
+SELECT TName
+FROM Teacher
+
